26cb45efcd09248fda115f0e16985babbeb03817
imkuldeepahlawat <imkuldeepahlawat@gmail.com>
2023-12-13 20:50:57 +0530

    paint working , colabration is painding

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Tldraw } from "@tldraw/tldraw";
+import "@tldraw/tldraw/tldraw.css";
+import "./App.css";
+
+function App() {
+  return (
+    <div style={{ position: "fixed", inset: 0 }}>
+      <Tldraw />
+    </div>
+  );
+}
+
+export default App;
